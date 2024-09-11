19f4a302bc6103a6df2032975d1ef40f879a3577
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-09-11 19:16:51 -0300

    added active model serializers for contacts

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,20 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthdate, :phones, :address, :kind
+  #Associations
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+  
+  # Utiliza do mesmo jeito que era usado no model, mas substitui  'as_json' por 'attributes' e 'options' por '*args' e 'self' por 'object'
+#   def attributes(*args)
+#   h = super
+#   h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+#   h
+#  end
+  def attributes(*args)
+  h = super
+  h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+  h
+ end
 end
+
