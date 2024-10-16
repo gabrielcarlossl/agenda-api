a07d5fb5e2182bdd576b13c2c5db58281e0e45ea
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-10-16 19:12:23 -0300

    feat: ensure api request with vnd.api+json type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+ # Bloqueia a API para receber apenas se no conteudo do HEADER  tiver algo com 'vnd\.api\+json' de acordo com a documentação, se tiver ele continua
+ # se não tiver ele vai retornar nada e vai exibir o status 406
+
+  before_action :ensure_json_request
+
+  def ensure_json_request
+    return if request.headers["Accept"] =~ /vnd\.api\+json/
+    render :nothing => true, :status => 406
+  end
+
 end
